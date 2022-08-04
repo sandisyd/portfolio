00e5d1da71dfb8187f396f4bcd468eeab646417a
Sandi <33063833+sandisyd@users.noreply.github.com>
2022-08-04 07:26:14 +0700

    add parallax effect

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //event pada saat link di klik
 
-$('.page-scroll').on('click', function(){
+$('.page-scroll').on('click', function () {
     var href = $(this).attr('href');
     var elementHref = $(href);
 
@@ -10,3 +10,36 @@ $('.page-scroll').on('click', function(){
 
 });
 
+
+//paralax
+
+//about
+$(window).on('load', function(){
+    $('.kiri').addClass('pMuncul');
+    $('.kanan').addClass('pMuncul');
+})
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+    });
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 3 + '%)'
+    });
+    $('.jumbotron p').css({
+        'transform': 'translate(0px, ' + wScroll / 1.5 + '%)'
+    });
+
+    //Portfolio
+    if (wScroll > $('.portfolio').offset().top - 250) {
+        $('.portfolio .thumbnail').each(function (i) {
+            setTimeout(
+                function () {
+
+                    $('.portfolio .thumbnail').eq(i).addClass('muncul');
+                }, 300 * (i + 1)
+            );
+        });
+    }
+});
